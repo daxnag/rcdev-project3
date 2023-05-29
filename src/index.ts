23fae3522c51e55cb02bdce4524d11f6796e5626
Dax Panganiban <daxpanganiban@rcadmins-Laptop-4.local>
2023-05-29 18:41:37 +0800

    Annotations

--- src/index.ts
+++ src/index.ts
@@ -20,10 +20,14 @@ import {
     ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
 
     IViewerPlugin,
-
     // Color, // Import THREE.js internals
     // Texture, // Import THREE.js internals
+    Object3D
 } from "webgi";
+
+import gsap from 'gsap';
+import  * as THREE  from 'three';
+
 import "./styles.css";
 
 async function setupViewer(){
@@ -35,7 +39,9 @@ async function setupViewer(){
 
     // Add some plugins
     const manager = await viewer.addPlugin(AssetManagerPlugin)
-
+    const camera = viewer.scene.activeCamera
+    const position = camera.position
+    const target = camera.target
     // Add a popup(in HTML) with download progress when any asset is downloading.
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
 
@@ -58,12 +64,95 @@ async function setupViewer(){
     await addBasePlugins(viewer)
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
+    //await viewer.addPlugin(CanvasSnipperPlugin)
 
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
+    const gltfAnims = viewer.getPlugin(GLTFAnimationPlugin);
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+// Play all animations
+
+    await manager.addFromPath("./assets/house.glb")
+
+    await timeout(1000) // wait 1 sec
+    
+    const tl = gsap.timeline();
+    let needsUpdate = true;
+
+    const canvas = document.getElementById('number');
+    
+    const ctx = canvas.getContext('2d');
+
+    const x = 32;
+    const y = 32;
+    const radius = 30;
+    const startAngle = 0;
+    const endAngle = Math.PI * 2;
+
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.fill();
+
+    ctx.strokeStyle = 'rgb(255, 255, 255)';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.stroke();
+
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.font = '32px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('1', x, y);
+
+    const numberTexture = new THREE.CanvasTexture(
+        document.querySelector('#number')
+    );
+    
+    const spriteMaterial = new THREE.SpriteMaterial({
+        map: numberTexture,
+        alphaTest: 0.5,
+        transparent: true,
+        depthTest: false,
+        depthWrite: false
+    });
+    
+    const sprite = new Object3D(spriteMaterial);
+    sprite.position.set(250, 250, 250);
+    sprite.scale.set(35, 35, 1);
+
+
+    viewer.scene.setDirty();
+
+    document.querySelector('.explore')?.addEventListener('click', () => {
+        tl.to(position, {x: -6.3269533864, y: 4.0538719781 , z: -7.0667129458, duration: 1, onUpdate})
+        tl.to(target, {x: 0.4695225008, y: 0.4086792986, z: 0.0404471063, duration: 0.5, onUpdate})
+        expandHouse();
+    })
+
+    async function expandHouse() {
+        gltfAnims.animationSpeed = 2;
+        gltfAnims.loopAnimations = false;
+
+        await timeout(2500)
+        await gltfAnims.playAnimation() ;
+    }
+
+    
+    function onUpdate() {
+        needsUpdate = true;
+        //viewer.renderer.resetShadows();
+        viewer.setDirty()
+    }
+
+    viewer.addEventListener('preFrame', () => {
+        if(needsUpdate) {
+            camera.positionUpdated(true);
+            camera.targetUpdated(true);
+            needsUpdate = false;    
+        }
+    })
 
     // Load an environment map if not set in the glb file
     // await viewer.scene.setEnvironment(
@@ -73,9 +162,9 @@ async function setupViewer(){
     // );
 
     // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
+    //const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
     // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+    //uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
 
 }
 
